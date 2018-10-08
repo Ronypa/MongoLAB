d5141b1a48025dbb9522aa072aec363dcc163004
Ronypa <ronxrpaniaguach@gmail.com>
2018-10-08 14:28:39 -0600

    Creacion del proyecto

---
+++ MongoLAB/Form1.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MongoLAB
+{
+    public partial class Form1 : Form
+    {
+        MongoClient client = new MongoClient("mongodb://localhost:27017");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
